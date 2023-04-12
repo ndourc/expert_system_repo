83c94efffe35e2c7fe66e5a10d9ec67a4aa2f8e4
ndourc <119441286+ndourc@users.noreply.github.com>
2023-04-12 14:39:08 +0200

    Update expert_system.pl

--- expert_system.pl
+++ expert_system.pl
@@ -28,3 +28,13 @@ daughter(X,Y) :- child(X,Y), female(X).
 surviving_spouse(X) :- husband(X,Y), not dead(Y), !.
 surviving_child(X) :- child(X,Y), not dead(Y), !.
 eligible_heir(X) :- surviving_spouse(_), surviving_child(X), not child(X,_), !.
+
+/*This script defines some facts, indexes, and rules related to family relationships, such as spouse, parent, child, son, daughter, surviving spouse, surviving child, and eligible heir.
+
+To query this script, you can use the Prolog query language. Here are some examples:
+
+To find out who is married to Jane, you can type: spouse(john, jane).
+To find out who is the son of Kate, you can type: son(X, kate).
+To find out who is the surviving spouse, you can type: surviving_spouse(X).
+To find out who is an eligible heir, you can type: eligible_heir(X).
+You can also use logical operators such as AND (",") and OR (";") to combine queries. For example, to find out who is the son or daughter of John and Jane, you can type: (son(X, john) ; daughter(X, jane))*/
